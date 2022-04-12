fad3d7fbd7656f0a7418d635ce2af3ea939436c4
BeatriceWambuiMbugua <wambuimbeatrice@gmail.com>
2022-04-12 08:04:57 +0300

    add date pipe directive

--- src/app/goal.ts
+++ src/app/goal.ts
@@ -4,7 +4,8 @@ export class Goal {
   constructor(
     public id: number,
     public name: string,
-    public description: string
+    public description: string,
+    public completeDate: Date
   ) {
     this.showDescription = false;
   }
