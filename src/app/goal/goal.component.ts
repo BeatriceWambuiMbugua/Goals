c73201200b1a7af3bbeb0466973a90620ca24a50
BeatriceWambuiMbugua <wambuimbeatrice@gmail.com>
2022-04-12 08:09:35 +0300

    add the Date() instance that takes 3 arguments year, month, day

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -10,11 +10,21 @@ export class GoalComponent implements OnInit {
   // displaying goals in Array
 
   goals: Goal[] = [
-    new Goal(1, 'watching finding Nemo', 'finding it on Netflix'),
-    new Goal(2, 'coding Angular', 'reading on Canva'),
-    new Goal(3, 'working in pairs', 'meeting online'),
-    new Goal(4, 'It is easier as it seems', "this is William's Mantra"),
-    new Goal(5, 'to solve the blockers', 'as a group'),
+    new Goal(
+      1,
+      'watching finding Nemo',
+      'finding it on Netflix',
+      new Date(2022, 3, 12)
+    ),
+    new Goal(2, 'coding Angular', 'reading on Canva', new Date(2022, 3, 11)),
+    new Goal(3, 'working in pairs', 'meeting online', new Date(2022, 3, 11)),
+    new Goal(
+      4,
+      'It is easier as it seems',
+      "this is William's Mantra",
+      new Date(2022, 3, 9)
+    ),
+    new Goal(5, 'to solve the blockers', 'as a group', new Date(2022, 3, 10)),
   ];
 
   toggleDetails(index: number) {
